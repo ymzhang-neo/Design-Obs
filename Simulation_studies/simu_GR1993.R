fc7ed1fff805f1f3d92625b4d692bfa3126b2e12
ymzhang-neo <ymzhang.neo@outlook.com>
2019-04-25 10:08:30 -0400

    Simulation studies in Gu and Rosenbaum (1993)

---
+++ Simulation_studies/simu_GR1993.R
@@ -0,0 +1,11 @@
+# Simulation: GR1993
+# Xing Sam Gu & Paul R. Rosenbaum (1993) 
+# Comparison of Multivariate Matching Methods: Structures, Distances, and Algorithms
+# Journal of Computational and Graphical Statistics, 2:4, 405-420
+# DOI: 10.1080/10618600.1993.10474623
+
+
+# Overview ----------------------------------------------------------------
+
+# The article designed simulation settings to investigate three aspects of 
+# matching procedures: algorithm, structure, and distance.
