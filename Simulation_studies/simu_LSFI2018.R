4dfd1d78767f612cd6ccd92afebb8cedd3294139
ymzhang-neo <ymzhang.neo@outlook.com>
2020-04-20 19:31:30 -0400

    Update the "format" (LSFI2018)

--- Simulation_studies/simu_LSFI2018.R
+++ Simulation_studies/simu_LSFI2018.R
@@ -1,55 +1,106 @@
-# Simulation: LSFI2018
 # Lu, Sadiq, Feaster, Ishwaran (2018)
 # Estimating individual treatment effect in observational data using random forest methods
 # Journal of Computational and Graphical Statistics
 
-set.seed(1)
+# Parameters --------------------------------------------------------------
 
-para_N = 500 # 5000
+n_sample = 5000 # 500, 5000
+n_simu = 250 # 1000, 250 (corresponding to n_sample)
 
-para_beta_W = c(-2, 0.028, -0.374, -0.03, 0.118, -0.394, 0.875, 0.9)
+para_PS_fn = function(X) {
+  # input: covariate vector X (length 20)
+  plogis( # inverse logit
+    -2 + 0.028 * X[1] - 0.374 * X[2] - 0.03 * X[3] + 0.118 * X[4] - 
+    0.394 * X[11] + 0.875 * X[12] + 0.9 * X[13]
+  ) 
+}
+
+para_Y_mean_fn_g = function(X) {
+  # input: covariate vector X (length 20)
+  0.254 * (X[2]^2) - 0.152 * X[11] - 0.4 * (X[11]^2) - 0.126 * X[12]
+}
+
+para_Y_mean_fn_h = function(X) {
+  # input: covariate vector X (length 20)
+  0.254 * (X[3]^2) - 0.152 * X[4] - 0.126 * X[5] - 0.4 * (X[5]^2)
+}
+
+para_Y_mean_fn_1 = function(X, W) {
+  # input: covariate vector X (length 20)
+  # input: treatment status W (0, 1)
+  2.455 + 
+    (-1) * (1 - W) * (0.4 * X[1] + 0.154 * X[2] - 0.152 * X[11] - 0.126 * X[12]) + 
+    (-1) * W * (para_Y_mean_fn_g(X) > 0) 
+}
+
+para_Y_mean_fn_2 = function(X, W) {
+  # input: covariate vector X (length 20)
+  # input: treatment status W (0, 1)
+  2.455 + 
+    (-1) * (1 - W) * sin(0.4 * X[1] + 0.154 * X[2] - 0.152 * X[11] - 0.126 * X[12]) + 
+    (-1) * W * (para_Y_mean_fn_g(X) > 0)  
+}
+
+para_Y_mean_fn_3 = function(X, W) {
+  # input: covariate vector X (length 20)
+  # input: treatment status W (0, 1)
+  2.455 + 
+    (-1) * (1 - W) * sin(0.4 * X[1] + 0.154 * X[2] - 0.152 * X[11] - 0.126 * X[12]) + 
+    (-1) * W * (para_Y_mean_fn_h(X) > 0)  
+}
+
+para_Y_sigma_error = 0.1
+
+
+# Simulation one dataset --------------------------------------------------
+
+set.seed(20200415 + 2259)
 
 dat_X = cbind(
-  matrix(rnorm(para_N * 11), ncol = 11), 
-  matrix(rbinom(para_N * 9, size = 1, prob = 0.5), ncol = 9)
+  matrix(rnorm(n_sample * 11), ncol = 11), 
+  matrix(rbinom(n_sample * 9, size = 1, prob = 0.5), ncol = 9)
 )
-colnames(dat_X) = paste0('X', 1:20)
+colnames(dat_X) = sprintf("X%02d", 1:20)
 
-dat_W = rbinom(para_N, size = 1, 
-               prob = 1/(1 + exp(- (dat_X[, paste0('X', c(1, 2, 3, 4, 11, 12, 13))] %*% para_beta_W[-1] + para_beta_W[1]))))
+dat_PS = apply(dat_X, 1, para_PS_fn)
+dat_W = rbinom(n_sample, size = 1, prob = dat_PS)
+
+# Function 1
+dat_Y_mean_fn_choice = para_Y_mean_fn_1
+dat_Y0 = apply(dat_X, 1, dat_Y_mean_fn_choice, W = 0)
+dat_Y1 = apply(dat_X, 1, dat_Y_mean_fn_choice, W = 1)
+# dat_Y_obs = mapply(dat_Y_mean_fn_choice, as.data.frame(t(dat_X)), dat_W) + 
+#   rnorm(n_sample, sd = para_Y_sigma_error)
+
+dat_Y_obs = (1 - dat_W) * dat_Y0 + dat_W * dat_Y1 + 
+  rnorm(n_sample, sd = para_Y_sigma_error)
+dat_Y = cbind(dat_Y0, dat_Y1, dat_Y_obs)
+colnames(dat_Y) = c('Y0', 'Y1', 'Y')
+
+dat = data.frame(dat_X, PS = dat_PS, W = dat_W, dat_Y)
 
 
-# Simulate outcomes -------------------------------------------------------
+# Estimate without design (D0) --------------------------------------------
 
-para_epsilon_sd = 0.1
+D00_est_ATE = mean(dat[, 'Y1']) - mean(dat[, 'Y0'])
 
-fn_Y1 = function(X = dat_X, W = dat_W) {
-  myY = rep(NA, para_N)
-  myY[dat_W == 0] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - dat_X[, paste0('X', c(1, 2, 11, 12))] %*% c(0.4, 0.154, -0.152, -0.126)
-  myY[dat_W == 1] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - ((0.254*(dat_X[, 'X2'])^2 - 0.152*dat_X[, 'X11'] - 0.4*dat_X[, 'X11']^2 - 0.126*dat_X[, 'X12']) > 0)
-  return(myY)
-}
+#### D0 Neyman ####
 
-fn_Y2 = function(X = dat_X, W = dat_W) {
-  myY = rep(NA, para_N)
-  myY[dat_W == 0] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - sin(dat_X[, paste0('X', c(1, 2, 11, 12))] %*% c(0.4, 0.154, -0.152, -0.126))
-  myY[dat_W == 1] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - ((0.254*(dat_X[, 'X2'])^2 - 0.152*dat_X[, 'X11'] - 0.4*dat_X[, 'X11']^2 - 0.126*dat_X[, 'X12']) > 0)
-  return(myY)
-}
+D0_estN_est = mean(dat[dat$W == 1, 'Y']) - mean(dat[dat$W == 0, 'Y'])
 
-fn_Y3 = function(X = dat_X, W = dat_W) {
-  myY = rep(NA, para_N)
-  myY[dat_W == 0] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - sin(dat_X[, paste0('X', c(1, 2, 11, 12))] %*% c(0.4, 0.154, -0.152, -0.126))
-  myY[dat_W == 1] = rnorm(para_N, sd = para_epsilon_sd) + 
-    2.455 - ((0.254*(dat_X[, 'X3'])^2 - 0.152*dat_X[, 'X4'] - 0.4*dat_X[, 'X5']^2 - 0.126*dat_X[, 'X5']) > 0)
-  return(myY)
-}
+D0_estN_var_W1 = var(dat[dat$W == 1, 'Y'])
+D0_estN_var_W0 = var(dat[dat$W == 0, 'Y'])
+D0_estN_var = D0_estN_var_W1 / sum(dat$W == 1) + 
+  D0_estN_var_W0 / sum(dat$W == 0)
 
-dat_Y = fn_Y1(X = dat_X, W = dat_W)
-# dat_Y = fn_Y2(X = dat_X, W = dat_W)
-# dat_Y = fn_Y3(X = dat_X, W = dat_W)
+D0_estN_CI = D0_estN_est + c(-1, 1) * 1.96 * sqrt(D0_estN_var)
+
+#### D0 Regression (linear, main) ####
+
+D0_estR1_fit = lm(Y ~ . - PS - Y0 - Y1, data = dat)
+D0_estR1_est = coef(summary(D0_estR1_fit))['W', 'Estimate']
+D0_estR1_CI = D0_estR1_est + c(-1, 1) * 
+  1.96 * coef(summary(D0_estR1_fit))['W', 'Std. Error']
+
+
+
